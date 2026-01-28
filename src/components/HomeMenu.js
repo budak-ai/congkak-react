e25e622d9ddea49eb61eb17d81bab5df3baa9385
Budak <ayuinmetaland@gmail.com>
2026-01-28 09:46:48 +0000

    revamp: minimal dark gold homepage, remove board graphic

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -23,27 +23,19 @@ const HomeMenu = ({ onPlay, onRules, onSettings, isOverlay = false, onClose }) =
       className={`home-menu ${isOverlay ? 'home-menu--overlay' : 'home-menu--fullscreen'}`}
       onClick={handleOverlayClick}
     >
+      <div className="home-menu__bg-glow" />
+      <div className="home-menu__bg-glow home-menu__bg-glow--2" />
+
       <div className="home-menu__content">
         <div className="home-menu__header">
+          <div className="home-menu__ornament">✦</div>
           <h1 className="home-menu__title">{t('menu.title', language)}</h1>
+          <div className="home-menu__divider">
+            <span className="home-menu__divider-line" />
+            <span className="home-menu__divider-dot">◈</span>
+            <span className="home-menu__divider-line" />
+          </div>
           <p className="home-menu__subtitle">{t('menu.subtitle', language)}</p>
-        </div>
-
-        <div className="home-menu__board-graphic">
-          <div className="home-menu__holes-row">
-            {[...Array(7)].map((_, i) => (
-              <div key={`top-${i}`} className="home-menu__hole" />
-            ))}
-          </div>
-          <div className="home-menu__houses">
-            <div className="home-menu__house" />
-            <div className="home-menu__house" />
-          </div>
-          <div className="home-menu__holes-row">
-            {[...Array(7)].map((_, i) => (
-              <div key={`bottom-${i}`} className="home-menu__hole" />
-            ))}
-          </div>
         </div>
 
         {!isOverlay && (
@@ -65,7 +57,7 @@ const HomeMenu = ({ onPlay, onRules, onSettings, isOverlay = false, onClose }) =
 
         <nav className="home-menu__nav">
           <button className="home-menu__button home-menu__button--primary" onClick={handlePlay}>
-            {t('menu.play', language)}
+            <span className="home-menu__button-text">{t('menu.play', language)}</span>
           </button>
           <button className="home-menu__button" onClick={onRules}>
             {t('menu.rules', language)}
